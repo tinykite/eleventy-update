c99aa769bd08b6a2d88c6500b0b0a7693d4ec1f2
tinykite <dakota@tinykitelab.com>
2020-11-15 14:54:07 -0600

    Fix post + case study styling

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,22 @@
+// Sorting
 const sortByDisplayOrder = require("./src/utils/sort-by-display-order.js");
+
+// Date filtering
+// TODO: Replace Moment
+const dateFilter = require("./src/filters/date-filter.js");
+const w3DateFilter = require("./src/filters/w3-date-filter.js");
+
+// Code Formatting
+// https://www.11ty.dev/docs/plugins/syntaxhighlight/
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
+// CSS compilation relies on PostCSS config, in css/styles.11ty.js
+// h/t Phil Hawksworth for how to include PostCSS without a separate build tool
+
 module.exports = (config) => {
+  // Add filters
+  config.addFilter("dateFilter", dateFilter);
+  config.addFilter("w3DateFilter", w3DateFilter);
   // Set directories to pass through to the dist folder
   config.addPassthroughCopy("./src/images/");
   config.addPassthroughCopy("./src/archive/");
