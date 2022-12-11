83129d9abfe28d88c3b8c4ef210af0c7d33b10cf
Dakota Sexton <dakota@tinykitelab.com>
2022-12-11 17:29:47 -0600

    fix: Missing active state in nav

--- src/_data/helpers.js
+++ src/_data/helpers.js
@@ -10,11 +10,18 @@ module.exports = {
   getLinkActiveState(itemUrl, pageUrl) {
     let response = "";
 
+    // if the item is the current page, add aria-current
     if (itemUrl === pageUrl) {
       response = ' aria-current="page"';
     }
 
+    // if the item is a parent of the current page, add data-state
     if (itemUrl.length > 1 && pageUrl.indexOf(itemUrl) === 0) {
+      response += ' data-state="active"';
+    }
+
+    // Add data-state to "Portfolio" nav item for all individual portfolio pages
+    if (pageUrl.includes("portfolio") && itemUrl === "/") {
       response += ' data-state="active"';
     }
 
